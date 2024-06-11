f28e7f0cab6f2f89ca2b17de3fc5ce8bf4014548
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-06-11 17:26:35 +0500

    Add files via upload

---
+++ approveExample.ts
@@ -0,0 +1,20 @@
+import {morphWalletClient, account} from "./help";
+import {morph} from "./chain/morphChain";
+import {parseEther, toHex} from "viem";
+
+
+//usdt address - 0x9E12AD42c4E4d2acFBADE01a96446e48e6764B98
+//uni address - 0x340Bad9627Cb72d1c4cC92c7F53c4995454130Ae
+//weth address - 0x5300000000000000000000000000000000000011
+
+
+async function withdrawMorph() {
+    const hash = await morphWalletClient.sendTransaction({
+        account,
+        chain: morph,
+        to: "0x340Bad9627Cb72d1c4cC92c7F53c4995454130Ae", //changes address for token
+        value: BigInt('0'),
+        data: toHex('0x095ea7b300000000000000000000000051be46170069a77050be5273d34e6155fdb7e031ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'),  //change address
+    });
+    console.log(hash)
+}
